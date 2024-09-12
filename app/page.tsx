e51f1354309d411a16a5a20ce9687f1a6ae5b9be
crypto0627 <jake0627a1@gmail.com>
2024-09-12 09:22:14 +0800

    remove: footer sitemap

--- app/page.tsx
+++ app/page.tsx
@@ -30,11 +30,10 @@ export default function Component() {
           <LatestNews />
         </section>
       </main>
-      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+      <footer className="flex flex-col justify-center gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-gray-500 dark:text-gray-400">
           © 2024 XueDAO organization. All rights reserved.
         </p>
-        <a href="https://www.xuedao.xyz/sitemap.xml">Sitemap</a>
       </footer>
     </div>
   );
